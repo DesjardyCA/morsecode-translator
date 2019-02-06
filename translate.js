894fd02ab214087f25ee1e3ea6d7257b5aaaffd0
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-05 18:09:42 -0800

    finalized translation of morse -> text, project complete

--- translate.js
+++ translate.js
@@ -20,7 +20,7 @@ function translateMorse(text) {
     var out = "";
     // if morse is passed in
     for (character of text) {
-        out += morse(character) + " ";
+        out += morse(character);
     }
 
     return out;
@@ -39,14 +39,62 @@ function translateText(text) {
 // will need to change to fix working with morse code (multiple char's for single letter)
 function morse(str) {
     switch (str) {
-        case "/":
-            return " ";
         case ".-":
             return "a";
         case "-...":
             return "b";
         case "-.-.":
             return "c";
+        case "-..":
+            return "d";
+        case ".":
+            return "e";
+        case "..-.":
+            return "f";
+        case "--.":
+            return "g";
+        case "....":
+            return "h";
+        case "..":
+            return "i";
+        case ".---":
+            return "j";
+        case "-.-":
+            return "k";
+        case ".-..":
+            return "l";
+        case "--":
+            return "m";
+        case "-.":
+            return "n";
+        case "---":
+            return "o";
+        case ".--.":
+            return "p";
+        case "--.-":
+            return "q";
+        case ".-.":
+            return "r";
+        case "...":
+            return "s";
+        case "-":
+            return "t";
+        case "..-":
+            return "u";
+        case "...-":
+            return "v";
+        case ".--":
+            return "w";
+        case "-..-":
+            return "x";
+        case "-.--":
+            return "y";
+        case "--..":
+            return "z";
+        case "/":
+            return " ";
+        default:
+            return "";
     }
 }
 
@@ -114,3 +162,4 @@ console.log(translate("Test") === "- . ... -");
 console.log(translate("Test test") === "- . ... - / - . ... -");
 // test morse -> english
 console.log(translate("... --- ...") === 'sos');
+console.log(translate("... --- ... / ... --- ...") === 'sos sos');
