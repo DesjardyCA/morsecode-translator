b861b1f6117e3be0723beafddda7bd1f9db44ffd
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-06 17:28:12 -0800

    converting to use object properties to store dictionary

--- translate.js
+++ translate.js
@@ -19,7 +19,7 @@ function translate(text) {
 function translateMorse(text) {
   var out = "";
   text.forEach(character => {
-    out += morse(character);
+    out += getKeyByValue(dictionary, morse(character));
   });
 
   return out;
@@ -28,130 +28,43 @@ function translateMorse(text) {
 function translateText(text) {
   var out = "";
   text.forEach(character => {
-    out += alpha(character) + " ";
+    out += dictionary[character] + " ";
   });
 
   return out.slice(0, -1);
 }
 
-function morse(str) {
-  switch (str) {
-    case ".-":
-      return "a";
-    case "-...":
-      return "b";
-    case "-.-.":
-      return "c";
-    case "-..":
-      return "d";
-    case ".":
-      return "e";
-    case "..-.":
-      return "f";
-    case "--.":
-      return "g";
-    case "....":
-      return "h";
-    case "..":
-      return "i";
-    case ".---":
-      return "j";
-    case "-.-":
-      return "k";
-    case ".-..":
-      return "l";
-    case "--":
-      return "m";
-    case "-.":
-      return "n";
-    case "---":
-      return "o";
-    case ".--.":
-      return "p";
-    case "--.-":
-      return "q";
-    case ".-.":
-      return "r";
-    case "...":
-      return "s";
-    case "-":
-      return "t";
-    case "..-":
-      return "u";
-    case "...-":
-      return "v";
-    case ".--":
-      return "w";
-    case "-..-":
-      return "x";
-    case "-.--":
-      return "y";
-    case "--..":
-      return "z";
-    case "/":
-      return " ";
-    default:
-      return "";
-  }
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value);
 }
 
-function alpha(char) {
-  switch (char) {
-    case "a":
-      return ".-";
-    case "b":
-      return "-...";
-    case "c":
-      return "-.-.";
-    case "d":
-      return "-..";
-    case "e":
-      return ".";
-    case "f":
-      return "..-.";
-    case "g":
-      return "--.";
-    case 'h':
-      return "....";
-    case 'i':
-      return "..";
-    case 'j':
-      return ".---";
-    case 'k':
-      return "-.-";
-    case 'l':
-      return ".-..";
-    case 'm':
-      return "--";
-    case 'n':
-      return "-.";
-    case 'o':
-      return "---";
-    case 'p':
-      return ".--.";
-    case 'q':
-      return "--.-";
-    case 'r':
-      return ".-.";
-    case 's':
-      return "...";
-    case 't':
-      return "-";
-    case 'u':
-      return "..-";
-    case 'v':
-      return "...-";
-    case 'w':
-      return ".--";
-    case 'x':
-      return "-..-";
-    case 'y':
-      return "-.--";
-    case 'z':
-      return "--..";
-    default:
-      return "/";
-  }
+var dictionary = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--.."
 }
 
 // test english -> morse
